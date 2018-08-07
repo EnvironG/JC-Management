28c40f9465401af4f7a23f6ab217357d5b8c7649
Jared Grant <jared.grant@canyonsdistrict.org>
2018-08-07 09:29:04 -0600

    Made Some buttons

--- src/components/formFields.js
+++ src/components/formFields.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
 
-    export class FormInput extends Component {
-        render() {
-            const { className, title, input, type, placeholder} = this.props;
-            return (
-                <div className='form-input'>
-                    <label className='form-input__title'>{title}</label>
-                    <input 
-                        className= {`${className} from-input__input`}
-                        type={type}
-                        {...input}
-                        placeholder={placeholder}
-                    />
+export class FormInput extends Component {
+    render() {
+        const { className, title, input, type, placeholder} = this.props;
+        return (
+            <div className={`${className} form-input`}>
+                <label className='form-input__title'>{title}</label>
+                <input 
+                    className= 'form-input__input'
+                    type={type}
+                    {...input}
+                    placeholder={placeholder}
+                />
 
-                </div>
-            )
-        }
+            </div>
+        )
     }
+};
+
+export class FormButton extends Component {
+    render() {
+        const { className, title, input, type, placeholder} = this.props;
+        return (
+            <div className={`${className} form-button`} >   
+                <button
+                    className='form-button__button'
+                    type={type}
+                    {...input}
+                    placeholder={placeholder}
+                    >
+                    {title}
+                    </button>
+
+            </div>
+        )
+    }
+}
